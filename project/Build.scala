fc3f0206eb0c2f5be885092f25ce75538cee48a8
romix <romixlev@gmail.com>
2013-09-20 16:33:18 +0200

    Use latest versions of Kryo and Akka. Try to support different versions of Scala

--- project/Build.scala
+++ project/Build.scala
@@ -20,26 +20,30 @@ import com.typesafe.sbt.osgi.SbtOsgi.{ OsgiKeys, osgiSettings, defaultOsgiSettin
 
 object MinimalBuild extends Build {
 
-  lazy val buildVersion = "0.3"
+  lazy val buildVersion = "0.3-SNAPSHOT"
 
   lazy val typesafe = "Typesafe Repository" at "http://repo.typesafe.com/typesafe/releases/"
   lazy val typesafeSnapshot = "Typesafe Snapshots Repository" at "http://repo.typesafe.com/typesafe/snapshots/"
+  lazy val sonatypeSnapshot = "Sonatype Snapshots Repository" at "https://oss.sonatype.org/content/repositories/snapshots/"
 
   lazy val root = Project(id = "akka-kryo-serialization", base = file("."), settings = Project.defaultSettings).settings(
     version := buildVersion,
     organization := "com.romix.akka",
     resolvers += typesafe,
     resolvers += typesafeSnapshot,
+    resolvers += sonatypeSnapshot,
     publishArtifact in packageDoc := false,
     crossScalaVersions := Seq("2.9.2", "2.9.3", "2.10.1"),
+    // crossScalaVersions := Seq("2.10.1"),
+    scalaVersion := "2.10.1",
     libraryDependencies += "com.typesafe.akka" % "akka-remote" % "2.0",
     libraryDependencies += "com.typesafe.akka" % "akka-kernel" % "2.0",
-    libraryDependencies += "com.esotericsoftware.kryo" % "kryo" % "2.21",
+    libraryDependencies += "com.esotericsoftware.kryo" % "kryo" % "2.22-SNAPSHOT",
     libraryDependencies += "com.novocode" % "junit-interface" % "0.8" % "test"
     )
     .settings(defaultOsgiSettings: _*)
     .settings(
-      OsgiKeys.exportPackage := Seq("com.romix.akka.serialization.kryo;version\"0.2.0.1\""),
+      OsgiKeys.exportPackage := Seq("com.romix.akka.serialization.kryo;version\"0.3.0.1\""),
       OsgiKeys.importPackage := Seq("com.esotericsoftware*;version=\"[2.20,3.0)\"",
         "com.typesafe.config;version=\"[0.4.1,1.0.0)\"",
         "akka*;version=\"[2.1.0,3.0.0)\"",
