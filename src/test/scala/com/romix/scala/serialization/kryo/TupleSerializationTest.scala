cca7a87c2f7bae9a71f95a7b848f31dfc9f5f006
romix <romixlev@gmail.com>
2013-09-22 23:04:15 +0200

    Fix unit test

--- src/test/scala/com/romix/scala/serialization/kryo/TupleSerializationTest.scala
+++ src/test/scala/com/romix/scala/serialization/kryo/TupleSerializationTest.scala
@@ -31,12 +31,12 @@ class TupleSerializationTest extends KryoTestCase {
 			    println("Test tuple serialization")
 				kryo.setRegistrationRequired(false)
 				// Support serialization of Scala collections
-//				kryo.register(classOf[scala.Tuple1[Any]],45)
-//				kryo.register(classOf[scala.Tuple2[Any,Any]],46)
-//				kryo.register(classOf[scala.Tuple3[Any,Any,Any]],47)
-//				kryo.register(classOf[scala.Tuple4[Any,Any,Any,Any]],48)
-//				kryo.register(classOf[scala.Tuple5[Any,Any,Any,Any,Any]],49)
-//				kryo.register(classOf[scala.Tuple6[Any,Any,Any,Any,Any,Any]],50)
+				kryo.register(classOf[scala.Tuple1[Any]],45)
+				kryo.register(classOf[scala.Tuple2[Any,Any]],46)
+				kryo.register(classOf[scala.Tuple3[Any,Any,Any]],47)
+				kryo.register(classOf[scala.Tuple4[Any,Any,Any,Any]],48)
+				kryo.register(classOf[scala.Tuple5[Any,Any,Any,Any,Any]],49)
+				kryo.register(classOf[scala.Tuple6[Any,Any,Any,Any,Any,Any]],50)
 				kryo.addDefaultSerializer(classOf[scala.Product], classOf[ScalaProductSerializer])
 				
 				roundTrip(14, (1,'2',"Three"))
@@ -48,19 +48,21 @@ class TupleSerializationTest extends KryoTestCase {
 				roundTrip(14, intTuple6)
 	 }
 
+
 	 def testTypedTupleSerialization():Unit = {
+	            import IntTuples._
 			    println("Test typed tuple serialization")
 				kryo.setRegistrationRequired(false)
 				kryo.addDefaultSerializer(classOf[scala.Product], classOf[ScalaProductSerializer])
 				
 				roundTrip(40, IntTuple10(1,2,3))
 				roundTrip(40, IntTuple10(1,2))
-//				roundTrip(40, IntTuple10("Name"))
 	 }
+
 }
 
-//class IntTuple10(_1:Int,_2:Int,_3:Int,_4:Int,_5:Int,_6:Int,_7:Int,_8:Int,_9:Int,_10:Int) extends Tuple10(_1,_2,_3,_4,_5,_6,_7,_8,_9,_10) {
-//}
+class IntTuple10(_1:Int,_2:Int,_3:Int,_4:Int,_5:Int,_6:Int,_7:Int,_8:Int,_9:Int,_10:Int) extends scala.Tuple10(_1,_2,_3,_4,_5,_6,_7,_8,_9,_10) {
+}
 
 //class IntTuple10(override val _1:Int,override val _2:Int,override val _3:Int,override val _4:Int,override val _5:Int,
 //		         override val _6:Int,override  val _7:Int,override  val _8:Int,override val _9:Int,override  val _10:Int) 
@@ -68,22 +70,33 @@ class TupleSerializationTest extends KryoTestCase {
 //	
 //}
 
-trait Debug {
-	def debug = println("DEBUG")
-}
 
 //class IntTuple10 
 //        extends Tuple10[Int, Int, Int, Int, Int, Int,Int, Int, Int, Int] {
 //}
 
+/*
 class MyTuple3[T1,T2,T3](__1:T1, __2:T2, __3:T3) extends Product3[T1,T2,T3] {
 	def _1 = __1
 	def _2 = __2
 	def _3 = __3
 	def canEqual(that:Any):Boolean = that.isInstanceOf[Product3[T1,T2,T3]]
 }
+*/
 
+
+package DebugUtils {
+    trait Debug {
+        def debug = println("DEBUG")
+    }
+}
+
+
+
+package IntTuples {
 object IntTuple10 {
+ import DebugUtils._
+
 //	type IntTuple10 = MyTuple3[Int, Int, Int]
 	type IntTuple3 = Tuple3[java.lang.Integer, java.lang.Integer, java.lang.Integer]
     type IntTuple2 = Tuple2[java.lang.Integer, Int]
@@ -106,6 +119,8 @@ object IntTuple10 {
 //		map
 //	}
 }
+}
+
 
 //class StringMap extends java.util.HashMap[String, String] {
 //	
