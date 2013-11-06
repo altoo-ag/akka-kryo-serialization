e04b15a05b8234558aa23ed841c6ce7a42cc4e3b
Roman Levenstein <romixlev@web.de>
2013-11-06 22:34:09 +0100

    Use a new groupId: "com.github.romix.akka" and add support for publishing the JARs in Sonatype Maven repos.

--- project/Build.scala
+++ project/Build.scala
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright 2012 Roman Levenstein
+ * Copyright 2013 Roman Levenstein
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -28,7 +28,7 @@ object MinimalBuild extends Build {
 
   lazy val root = Project(id = "akka-kryo-serialization", base = file("."), settings = Project.defaultSettings).settings(
     version := buildVersion,
-    organization := "com.romix.akka",
+    organization := "com.github.romix.akka",
     resolvers += typesafe,
     resolvers += typesafeSnapshot,
     resolvers += sonatypeSnapshot,
@@ -48,8 +48,43 @@ object MinimalBuild extends Build {
       "-target:jvm-1.6",
       "-language:_",
       "-Xlog-reflective-calls"
-    )
-    )
+    ),
+    
+    publishTo := {
+	val nexus = "https://oss.sonatype.org/"
+	if (buildVersion.trim.endsWith("SNAPSHOT"))
+	    Some("snapshots" at nexus + "content/repositories/snapshots")
+	else
+	    Some("releases" at nexus + "service/local/staging/deploy/maven2")
+	},
+
+    publishMavenStyle := true,
+
+    publishArtifact in Test := false,
+
+    pomIncludeRepository := { _ => false },
+
+    pomExtra := (
+  <url>https://github.com/romix/akka-kryo-serialization</url>
+  <licenses>
+    <license>
+      <name>The Apache Software License, Version 2.0</name>
+      <url>http://www.apache.org/licenses/LICENSE-2.0.txt</url>
+      <distribution>repo</distribution>
+    </license>
+  </licenses>
+  <scm>
+    <url>git@github.com:romix/akka-kryo-serialization.git</url>
+    <connection>scm:git:git@github.com:romix/akka-kryo-serialization.git</connection>
+  </scm>
+  <developers>
+    <developer>
+      <id>romix</id>
+      <name>Roman Levenstein</name>
+      <email>romixlev@gmail.com</email>
+    </developer>
+  </developers>
+    ))
     .settings(defaultOsgiSettings: _*)
     .settings(
       OsgiKeys.exportPackage := Seq("com.romix.akka.serialization.kryo;version\"0.3.0.1\""),
