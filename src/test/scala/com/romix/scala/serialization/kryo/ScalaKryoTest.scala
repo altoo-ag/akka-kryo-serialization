e26f62256e49e373c057ca7111068fbd8275c606
Ryan Bair <rbair@authorpub.com>
2014-09-11 10:55:53 -0400

    Override getDefaultSerializer to ensure that modules are serialized with the ObjectSerializer even if a different default serializer covers the  type. This fixes issues with equality between Nil and Kryo deserialized Nil.
    Fixes #37

---
+++ src/test/scala/com/romix/scala/serialization/kryo/ScalaKryoTest.scala
@@ -0,0 +1,17 @@
+package com.romix.scala.serialization.kryo
+
+import com.esotericsoftware.kryo.util.{DefaultClassResolver, DefaultStreamFactory, ListReferenceResolver}
+import org.scalatest.Outcome
+
+class ScalaKryoTest extends SpecCase {
+  kryo = new ScalaKryo(new DefaultClassResolver(), new ListReferenceResolver(), new DefaultStreamFactory())
+
+  "ScalaKryo" should "preserve Nil equality" in {
+    val deserializedNil = roundTrip(1, Nil)
+    assert(deserializedNil eq Nil)
+  }
+
+  override def withFixture(test: NoArgTest): Outcome = {
+    test()
+  }
+}
