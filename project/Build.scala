8bb1670b649e0c4a1f721f0f702a9a1d1876920a
Bharath Kumar <ee07b093@ee.iitm.ac.in>
2014-12-22 23:51:06 +0530

    Set current scala version to 2.11.2. Added shiro and commons dependencies for encryption/decryption services

--- project/Build.scala
+++ project/Build.scala
@@ -34,11 +34,14 @@ object MinimalBuild extends Build {
     resolvers += sonatypeSnapshot,
     // publishArtifact in packageDoc := false,
     crossScalaVersions := Seq("2.10.4","2.11.2"),
-    scalaVersion := "2.10.4",
+    scalaVersion := "2.11.2",
     libraryDependencies += "com.typesafe.akka" %% "akka-remote" % "2.3.3",
     libraryDependencies += "com.typesafe.akka" %% "akka-kernel" % "2.3.3",
     libraryDependencies += "com.esotericsoftware.kryo" % "kryo" % "2.24.0",
     libraryDependencies += "net.jpountz.lz4" % "lz4" % "1.2.0",
+    libraryDependencies += "org.apache.shiro" % "shiro-all" % "1.2.3",
+    libraryDependencies +=  "org.slf4j" % "slf4j-log4j12" % "1.7.7",
+    libraryDependencies += "commons-codec" % "commons-codec" % "1.9",
 
     libraryDependencies ++= {
       val sv = scalaVersion.value
