84a3080b2a755446c9c03e61994a020d6bd072a5
Bharath Kumar <Bharath.Lingannagari@ihealthtechnologies.com>
2014-12-31 15:43:21 +0530

    Added test to check if the AES key is readable from custom key class

---
+++ src/test/scala-2.11/com/romix/akka/serialization/kryo/AkkaKryoCryptoWithCustomKeyTests.scala
@@ -0,0 +1,81 @@
+package com.romix.akka.serialization.kryo
+
+import akka.actor.ActorSystem
+import akka.serialization.SerializationExtension
+import com.typesafe.config.ConfigFactory
+import org.scalatest.FlatSpec
+
+import scala.collection.mutable.AnyRefMap
+
+/**
+ * Created by blingannagari on 31/12/14.
+ */
+
+class KryoCryptoTestKey extends KryoCrypto {
+  def kryoAESKey = "TheTestSecretKey"
+}
+
+class AkkaKryoCryptoWithCustomKeyTests extends FlatSpec {
+  def testConfig(systemName: String, config: String): Unit = {
+    val system = ActorSystem("example", ConfigFactory.parseString(config))
+    // Get the Serialization Extension
+    val serialization = SerializationExtension(system)
+
+    s"$systemName" should "read the aes key from the custom class specified" in {
+      val atm = (List {
+        AnyRefMap[String, Any](
+          "foo" -> "foo",
+          "bar" -> "foo,bar,baz",
+          "baz" -> 124L)
+      }).toArray
+
+
+      val serializer = serialization.findSerializerFor(atm)
+      assert(serializer.isInstanceOf[KryoSerializer])
+      assert(serializer.asInstanceOf[KryoSerializer].aesKey == (new KryoCryptoTestKey).kryoAESKey)
+    }
+  }
+
+  testConfig("CustomAESKey", """
+      akka {
+        extensions = ["com.romix.akka.serialization.kryo.KryoSerializationExtension$"]
+        actor {
+          kryo {
+            type = "nograph"
+            idstrategy = "incremental"
+            kryo-reference-map = false
+            buffer-size = 65536
+            encryption = aes
+            custom-aeskey-class = "com.romix.akka.serialization.kryo.KryoCryptoTestKey"
+            implicit-registration-logging = true
+            mappings {
+              "scala.collection.immutable.HashMap$HashTrieMap"    = 30
+              "[Lscala.collection.immutable.HashMap$HashTrieMap;" = 31
+              "scala.collection.mutable.AnyRefMap"                = 34
+              "[Lscala.collection.mutable.AnyRefMap;"             = 35
+              "scala.collection.mutable.LongMap"                  = 36
+              "[Lscala.collection.mutable.LongMap;"               = 37
+              "[J" = 50
+              "[D" = 51
+              "[Z" = 52
+              "[Ljava.lang.Object;" = 53
+              "[Ljava.lang.String;" = 54
+              "scala.math.Ordering$String$" = 100
+            }
+
+          }
+         serializers {
+            kryo = "com.romix.akka.serialization.kryo.KryoSerializer"
+          }
+
+          serialization-bindings {
+            "scala.collection.mutable.AnyRefMap" = kryo
+            "[Lscala.collection.mutable.AnyRefMap;" = kryo
+            "scala.collection.mutable.LongMap" = kryo
+            "[Lscala.collection.mutable.LongMap;" = kryo
+          }
+        }
+      }
+                    """)
+
+}
