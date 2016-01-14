3b70e91ec60cc8a5da5b3e4829c343ba6583e247
Brian Topping <brian@mauswerks.net>
2016-01-14 06:08:26 -0700

    Update Test for #67 to show working `Vector`

--- src/test/scala/com/romix/akka/serialization/kryo/ByteStringTest.scala
+++ src/test/scala/com/romix/akka/serialization/kryo/ByteStringTest.scala
@@ -6,10 +6,11 @@ import akka.util.ByteString
 import com.esotericsoftware.kryo.util.{DefaultClassResolver, DefaultStreamFactory, ListReferenceResolver}
 import com.romix.scala.serialization.kryo.{ScalaKryo, SpecCase}
 import com.typesafe.config.ConfigFactory
-import org.scalatest.{Matchers, FlatSpec, Outcome}
+import org.scalatest.{WordSpecLike, Matchers, FlatSpec, Outcome}
 
-class ByteStringTest extends FlatSpec with Matchers {
-  val system = ActorSystem("example", ConfigFactory.parseString("""
+class ByteStringTest extends WordSpecLike with Matchers {
+  val system = ActorSystem("example", ConfigFactory.parseString(
+    """
 	akka {
 	  loggers = ["akka.event.Logging$DefaultLogger"]
 	  loglevel = "WARNING"
@@ -21,30 +22,49 @@ class ByteStringTest extends FlatSpec with Matchers {
 
     akka.actor.kryo {
       trace = true
-      idstrategy = "incremental"
+      idstrategy = "default"
       implicit-registration-logging = true
       post-serialization-transformations = off
     }
 
     akka.actor.serialization-bindings {
      "akka.util.ByteString$ByteString1C" = kryo
+     "akka.util.ByteString" = kryo
+     "scala.collection.immutable.Vector" = kryo
     }
-  """))
+    """))
   val serialization = SerializationExtension(system)
 
-  "ScalaKryo" should "handle compact ByteStrings" in {
-    val obj = ByteString("foo").compact
-    val serializer = serialization.findSerializerFor(obj)
-    Console.println("Object of class " + obj.getClass.getName + " got serializer of class " + serializer.getClass.getName)
-    serializer.getClass.equals(classOf[KryoSerializer]) should be(true)
-    // Check serialization/deserialization
-    val serialized = serialization.serialize(obj)
-    serialized.isSuccess should be(true)
+  "ScalaKryo" should {
+    "handle Vectors" in {
+      val obj = Vector("foo")
+      val serializer = serialization.findSerializerFor(obj)
+      Console.println("Object of class " + obj.getClass.getName + " got serializer of class " + serializer.getClass.getName)
+      serializer.getClass.equals(classOf[KryoSerializer]) should be(true)
+      // Check serialization/deserialization
+      val serialized = serialization.serialize(obj)
+      serialized.isSuccess should be(true)
 
-    val deserialized = serialization.deserialize(serialized.get, obj.getClass)
-    deserialized.isSuccess should be(true)
+      val deserialized = serialization.deserialize(serialized.get, obj.getClass)
+      deserialized.isSuccess should be(true)
 
-    deserialized.get.equals(obj) should be(true)
-    deserialized.getClass.equals(obj.getClass) should be(true)
+      deserialized.get.equals(obj) should be(true)
+      deserialized.get.getClass.equals(obj.getClass) should be(true)
+    }
+    "handle compact ByteStrings" in {
+      val obj = ByteString("foo").compact
+      val serializer = serialization.findSerializerFor(obj)
+      Console.println("Object of class " + obj.getClass.getName + " got serializer of class " + serializer.getClass.getName)
+      serializer.getClass.equals(classOf[KryoSerializer]) should be(true)
+      // Check serialization/deserialization
+      val serialized = serialization.serialize(obj)
+      serialized.isSuccess should be(true)
+
+      val deserialized = serialization.deserialize(serialized.get, obj.getClass)
+      deserialized.isSuccess should be(true)
+
+      deserialized.get.equals(obj) should be(true)
+      deserialized.get.getClass.equals(obj.getClass) should be(true)
+    }
   }
 }
