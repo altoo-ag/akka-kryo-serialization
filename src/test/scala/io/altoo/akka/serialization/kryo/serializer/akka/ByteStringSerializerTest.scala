bfdb3a3d6f206ed2c2cb1fceacd0e022fa93ac26
Nicolas Vollmar <nvo@scaling.ch>
2020-11-18 07:52:59 +0100

    cleans up ByteString serializer test

---
+++ src/test/scala/io/altoo/akka/serialization/kryo/serializer/akka/ByteStringSerializerTest.scala
@@ -0,0 +1,72 @@
+package io.altoo.akka.serialization.kryo.serializer.akka
+
+import akka.actor.ActorSystem
+import akka.serialization.SerializationExtension
+import akka.testkit.TestKit
+import akka.util.{ByteString, CompactByteString}
+import com.typesafe.config.ConfigFactory
+import io.altoo.akka.serialization.kryo.KryoSerializer
+import org.scalatest.flatspec.AnyFlatSpecLike
+import org.scalatest.matchers.should.Matchers
+
+object ByteStringSerializerTest {
+  private val testConfig =
+    """
+      |akka {
+      |  actor {
+      |    serializers {
+      |      kryo = "io.altoo.akka.serialization.kryo.KryoSerializer"
+      |    }
+      |    serialization-bindings {
+      |      "akka.util.ByteString$ByteString1C" = kryo
+      |      "akka.util.ByteString" = kryo
+      |    }
+      |  }
+      |}
+      |akka-kryo-serialization {
+      |  trace = true
+      |  id-strategy = "default"
+      |  implicit-registration-logging = true
+      |  post-serialization-transformations = off
+      |}
+      |""".stripMargin
+}
+
+class ByteStringSerializerTest extends TestKit(ActorSystem("testSystem", ConfigFactory.parseString(ByteStringSerializerTest.testConfig))) with AnyFlatSpecLike with Matchers {
+  private val serialization = SerializationExtension(system)
+
+
+  behavior of "ByteStringSerializer"
+
+  it should "handle ByteStrings" in {
+    val value = ByteString("foo")
+
+    // serialize
+    val serializer = serialization.findSerializerFor(value)
+    serializer shouldBe a[KryoSerializer]
+
+    val serialized = serialization.serialize(value)
+    serialized.isSuccess shouldBe true
+
+    // deserialize
+    val deserialized = serialization.deserialize(serialized.get, classOf[ByteString])
+    deserialized.isSuccess shouldBe true
+    deserialized.get shouldBe value
+  }
+
+  it should "handle compact ByteStrings" in {
+    val value = ByteString("foo").compact
+
+    // serialize
+    val serializer = serialization.findSerializerFor(value)
+    serializer shouldBe a[KryoSerializer]
+
+    val serialized = serialization.serialize(value)
+    serialized.isSuccess shouldBe true
+
+    // deserialize
+    val deserialized = serialization.deserialize(serialized.get, classOf[CompactByteString])
+    deserialized.isSuccess shouldBe true
+    deserialized.get shouldBe value
+  }
+}
