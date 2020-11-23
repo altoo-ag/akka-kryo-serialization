c08793bdc74fc90457ea0910ee12a93ee927c7b9
Nicolas Vollmar <nvo@scaling.ch>
2020-11-23 12:46:23 +0100

    adds proper terminate to prevent memory leaks

--- src/test/scala/io/altoo/akka/serialization/kryo/CryptoSerializationTest.scala
+++ src/test/scala/io/altoo/akka/serialization/kryo/CryptoSerializationTest.scala
@@ -3,6 +3,7 @@ package io.altoo.akka.serialization.kryo
 import akka.actor.ActorSystem
 import akka.serialization.SerializationExtension
 import com.typesafe.config.ConfigFactory
+import org.scalatest.BeforeAndAfterAll
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
@@ -11,40 +12,49 @@ import scala.collection.immutable.HashMap
 object CryptoSerializationTest {
   private val config =
     """
-    akka {
-      actor {
-        serializers {
-          kryo = "io.altoo.akka.serialization.kryo.KryoSerializer"
-        }
-        serialization-bindings {
-          "scala.collection.immutable.HashMap" = kryo
-          "[Lscala.collection.immutable.HashMap;" = kryo
-          "scala.collection.mutable.LongMap" = kryo
-          "[Lscala.collection.mutable.LongMap;" = kryo
-        }
-      }
-    }
-    akka-kryo-serialization {
-      post-serialization-transformations = aes
-      encryption {
-        aes {
-          key-provider = "io.altoo.akka.serialization.kryo.DefaultKeyProvider"
-          mode = "AES/GCM/PKCS5Padding"
-          iv-length = 12
-          password = "j68KkRjq21ykRGAQ"
-          salt = "pepper"
-        }
-      }
-    }
-    """
+      |akka {
+      |  actor {
+      |    serializers {
+      |      kryo = "io.altoo.akka.serialization.kryo.KryoSerializer"
+      |    }
+      |    serialization-bindings {
+      |      "scala.collection.immutable.HashMap" = kryo
+      |      "[Lscala.collection.immutable.HashMap;" = kryo
+      |      "scala.collection.mutable.LongMap" = kryo
+      |      "[Lscala.collection.mutable.LongMap;" = kryo
+      |    }
+      |  }
+      |}
+      |akka-kryo-serialization {
+      |  post-serialization-transformations = aes
+      |  encryption {
+      |    aes {
+      |      key-provider = "io.altoo.akka.serialization.kryo.DefaultKeyProvider"
+      |      mode = "AES/GCM/PKCS5Padding"
+      |      iv-length = 12
+      |      password = "j68KkRjq21ykRGAQ"
+      |      salt = "pepper"
+      |    }
+      |  }
+      |}
+      |""".stripMargin
 }
-class CryptoSerializationTest extends AnyFlatSpec with Matchers {
-  private val sourceSystem = ActorSystem("encrypted", ConfigFactory.parseString(CryptoSerializationTest.config))
-  private val targetSystem = ActorSystem("encrypted", ConfigFactory.parseString(CryptoSerializationTest.config))
+
+class CryptoSerializationTest extends AnyFlatSpec with Matchers with BeforeAndAfterAll {
+  private val sourceSystem = ActorSystem("source", ConfigFactory.parseString(CryptoSerializationTest.config))
+  private val targetSystem = ActorSystem("target", ConfigFactory.parseString(CryptoSerializationTest.config))
   private val sourceSerialization = SerializationExtension(sourceSystem)
   private val targetSerialization = SerializationExtension(targetSystem)
 
-  it should "serialize and deserialize with encryption" in {
+  override protected def afterAll(): Unit = {
+    sourceSystem.terminate()
+    targetSystem.terminate()
+  }
+
+
+  behavior of "Encrypted serialization"
+
+  it should "serialize and deserialize with encryption accross actor systems" in {
     val atm = List {
       HashMap[String, Any](
         "foo" -> "foo",
